ad21e84aaa0c7a233e2ac077608df3c626c3f0fa
Karrar <krara.2003@gmail.com>
2025-03-17 01:43:33 +0300

    changes

---
+++ lib/uplog_card.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class LogEntryCard extends StatelessWidget {
+  final LogEntry logEntry;
+
+  const LogEntryCard({super.key, required this.logEntry});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Card(
+      elevation: 5,
+      margin: const EdgeInsets.all(8),
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("On ${formatDate(logEntry.date)}",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+                color: theme.primaryColor,
+              ),
+            ),
+            const Divider(),
+            Text(
+              logEntry.content,
+              style: const TextStyle(fontSize: 14),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  String formatDate(String dateString) {
+    try {
+      DateTime date = DateTime.parse(dateString);
+      return DateFormat('yyyy-MM-dd:HH:mm').format(date);
+    } catch (e) {
+      return dateString; // Fallback if parsing fails
+    }
+  }
+}
+
+class LogEntry {
+  final String date;
+  final String content;
+
+  LogEntry({required this.date, required this.content});
+
+  factory LogEntry.fromJson(Map<String, dynamic> json) {
+    return LogEntry(
+      date: json['date'],
+      content: json['content'],
+    );
+  }
+}
---
+++ lib/uplogs_page.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+import 'package:mypharmacy/uplog_card.dart';
+import 'package:mypharmacy/user_state.dart';
+import 'package:provider/provider.dart';
+import 'api_call_manager.dart';
+
+
+class UpLogsPage extends StatefulWidget {
+  const UpLogsPage({super.key});
+
+  @override
+  UpLogsListState createState() => UpLogsListState();
+}
+
+class UpLogsListState extends State<UpLogsPage> {
+  static const int pageSize = 10;
+  String cursor = "2200-01-01";
+  final PagingController<int, LogEntry> pageCont = PagingController(firstPageKey: 0);
+
+  String from = "";
+  String to = "";
+
+  @override
+  void initState() {
+    super.initState();
+    pageCont.addPageRequestListener((pageKey) {
+      loadPage(pageKey);
+    });
+  }
+
+  void loadPage(int pageKey) async {
+    try {
+      final userState = context.read<UserState>();
+      final pharmaIndex = userState.pharmaIndex;
+
+      List<LogEntry> items = await fetchLogs(pharmaIndex: pharmaIndex);
+      final isLastPage = items.length < pageSize;
+
+      if (isLastPage) {
+        pageCont.appendLastPage(items);
+      } else {
+
+        cursor =items.last.date.split("+").first;
+        pageCont.appendPage(items,pageKey+1);
+      }
+    } catch (e) {
+      pageCont.error = e;
+    }
+  }
+
+  void update(String newFrom, String newTo) {
+    setState(() {
+      from = newFrom;
+      to = newTo;
+      cursor = "2020-01-01";
+    });
+    pageCont.refresh();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        PagedListView<int, LogEntry>(
+          pagingController: pageCont,
+          builderDelegate: PagedChildBuilderDelegate<LogEntry>(
+            itemBuilder: (context, item, index) => LogEntryCard(logEntry: item),
+          ),
+        ),
+        Positioned(
+          bottom: 20.0,
+          right: 20.0,
+          child: FloatingActionButton(
+            onPressed: () {
+              //search(context, update);
+            },
+            tooltip: "Search",
+            heroTag: null,
+            child: const Icon(Icons.picture_as_pdf),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    pageCont.dispose();
+    super.dispose();
+  }
+
+  Future<List<LogEntry>> fetchLogs({required int pharmaIndex}) async {
+    List<String> queryParams = [
+      "pharma_index=$pharmaIndex",
+      if (from.isNotEmpty) "from_date=$from",
+      if (to.isNotEmpty) "to_date=$to",
+      "cursor=$cursor",
+      "limit=$pageSize"
+    ];
+
+    String route = "$serverAddress/update_logs?${queryParams.join('&')}";
+    try {
+      final apiCaller = context.read<APICaller>();
+      final response = await apiCaller.get(route);
+
+      if (response.statusCode == 200) {
+        List<dynamic> jsonResponse = response.data;
+        return jsonResponse.map((jsonObject) => LogEntry.fromJson(jsonObject)).toList();
+      } else {
+        throw Exception('Failed to load data');
+      }
+    } catch (e) {
+      print('Error: $e');
+      throw Exception('Error fetching logs: $e');
+    }
+  }
+}
